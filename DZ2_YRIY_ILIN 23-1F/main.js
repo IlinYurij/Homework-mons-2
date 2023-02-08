3594ff5accee2a4f8784ce559e2ab846589f0ad0
IlinYurij <124767165+IlinYurij@users.noreply.github.com>
2023-02-08 14:09:06 +0600

    Сделал дз 2

---
+++ DZ2_YRIY_ILIN 23-1F/main.js
@@ -0,0 +1,17 @@
+var arr = [10, 20, 30, 50, 235, 3000];
+for (i of arr) {
+    if (String(i)[0] === '1' || String(i)[0] === '2' || String(i)[0] === '5') {
+        console.log(i);
+    }
+}
+
+for (var i = 20; i > 0; i--) {
+    console.log(i);
+}
+var input = prompt('Введите цвет')
+var obj = {red: "stop", green: "go", yellow: 'ready'}
+if (input in obj) {
+    alert(obj[input]);
+} else {
+    alert('error,write correct');
+}
